a75105ccd6e86b741d95ebdfd9157420a20a696d
DevWellington <devwellington@gmail.com>
2025-01-11 21:35:52 -0300

    Push main

--- php/run.sh
+++ php/run.sh
@@ -1,1 +1,3 @@
-docker run --rm --name laravel -p 8000:8000 devwellington/laravel:2025
+# docker run --rm --name laravel -p 8000:8000 devwellington/laravel:2025
+
+docker run --rm --name laravel -p 8000:8000 devwellington/laravel:latest
